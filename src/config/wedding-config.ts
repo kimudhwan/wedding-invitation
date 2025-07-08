6937ed2a7edbd83ebf4949a26072590de33be5f6
kimudhwan <112496089+kimudhwan@users.noreply.github.com>
2025-07-08 22:57:13 +0900

    Update wedding-config.ts
    현수 마태

--- src/config/wedding-config.ts
+++ src/config/wedding-config.ts
@@ -13,7 +13,7 @@ interface GalleryConfig {
 export const weddingConfig = {
   // 메타 정보
   meta: {
-    title: "신랑 ❤️ 신부의 결혼식에 초대합니다",
+    title: "현수 ❤️ 마태의 결혼식에 초대합니다",
     description: "결혼식 초대장",
     ogImage: "/images/ha0h-1fsi-bqt3.jpg",
     noIndex: true,
